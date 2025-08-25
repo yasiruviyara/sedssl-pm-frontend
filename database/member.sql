9f01c5c06f0ee26970db11a3be39a8c4f78a3b64
yasiruviyara <80908044+yasiruviyara@users.noreply.github.com>
2025-08-25 09:51:06 +0530

    chore: implement db project schema & update member schema

--- database/member.sql
+++ database/member.sql
@@ -1,25 +1,24 @@
+-- Enums
+CREATE TYPE gender_enum AS ENUM ('male', 'female', 'other');
+CREATE TYPE account_status_enum AS ENUM ('pending', 'under_review', 'approved', 'rejected');
+CREATE TYPE role_enum AS ENUM ('member', 'chapter-lead', 'division-head', 'board-member', 'admin');
+
 CREATE TABLE members (
   id BIGSERIAL PRIMARY KEY,
-  user_id UUID REFERENCES auth.users(id) UNIQUE NOT NULL,
+  user_id UUID UNIQUE NOT NULL REFERENCES auth.users(id),
   initials TEXT NOT NULL,
   full_name TEXT NOT NULL,
   phone TEXT NOT NULL,
-  gender TEXT CHECK (gender IN ('male', 'female', 'other')) NOT NULL,
+  gender gender_enum NOT NULL,
   dob DATE NOT NULL,
   cv TEXT NOT NULL,
   avatar TEXT,
-  avatar_visibility TEXT CHECK (
-    avatar_visibility IN ('public', 'private')
-  ) DEFAULT 'public',
-  account_status DEFAULT 'pending' CHECK (
-    account_status IN ('pending', 'under_review', 'approved', 'rejected')
-  ),  
+  avatar_visibility TEXT CHECK (avatar_visibility IN ('public', 'private')) DEFAULT 'public',
+  account_status account_status_enum DEFAULT 'pending',  
   submitted_at TIMESTAMPTZ DEFAULT now(),
   reviewed_at TIMESTAMPTZ,
   chapter TEXT, -- there is one value in an array
   division TEXT, -- there is one value in an array
-  role TEXT CHECK (
-    role IN ('member', 'chapter-lead', 'division-head', 'board-member', 'admin')
-  ) DEFAULT 'member',
+  role role_enum DEFAULT 'member',
   position TEXT, -- there is one value in an array
 );
